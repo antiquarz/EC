55775afa18b5ec0ccfaf42830f2d5062a48ad02a
Antiquarz <office@antiquarz.at>
2022-09-06 16:24:09 +0200

    all

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
 
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: *');
 
-header("Access-Control-Allow-Headers: X-Requested-With");?>
+header("Access-Control-Allow-Headers: *");?>
 
 
 <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
