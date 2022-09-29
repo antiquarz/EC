3eda0f11664c6eacf59a1c16c2f5d0238a2b0b25
Antiquarz <office@antiquarz.at>
2022-09-29 11:20:05 +0200

    eC first

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  
-})
+const options = {
+  key: fs.readFileSync('./localhost-key.pem'),
+  cert: fs.readFileSync('./localhost.pem')
+};
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use((req, res, next) => {
+  res.send('<h1>HTTPS is working!</h1>');
+});
+
+const port = 3000;
+
+https.createServer(options, app).listen(port, () => {
+  console.log('Server listening on port ' + port);
+});
